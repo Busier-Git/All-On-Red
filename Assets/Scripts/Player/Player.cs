5c10ac7529c732c4b6e441ae9c872d5a37f85ba9
Busier-Git <ignacio.penailillo@alumnos.ucn.cl>
2026-06-06 22:07:47 -0400

    fix: playerUI, UI

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [Header("Sistema de Vida")]
     public int vidaMaxima = 5;
     private int vidaActual;
+
+    [Header("UI Vidas")]
+    public UIVida uiVidas;
 
     [Header("Sistema de Disparo")]
     public GameObject prefabProyectil;
@@ -18,68 +21,29 @@ public class Player : MonoBehaviour
     void Start()
     {
         vidaActual = vidaMaxima;
+        if (uiVidas != null)
+            uiVidas.ActualizarVidas(vidaActual, vidaMaxima);
     }
 
-   void Update()
+    void Update()
     {
-        // 2. DISPARO CONTINUO CON FLECHAS
         if (Time.time >= tiempoSiguienteDisparo)
         {
-            // Disparos diagonales
-            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
-            {
-                Disparar(new Vector2(1, 1).normalized);
-            }
-            else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
-            {
-                Disparar(new Vector2(-1, 1).normalized);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
-            {
-                Disparar(new Vector2(1, -1).normalized);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
-            {
-                Disparar(new Vector2(-1, -1).normalized);
-            }
-
-            // Disparos simples
-            else if (Input.GetKey(KeyCode.UpArrow))
-            {
-                Disparar(Vector2.up);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                Disparar(Vector2.down);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                Disparar(Vector2.left);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                Disparar(Vector2.right);
-            }
+            if (Input.GetKey(KeyCode.UpArrow)) Disparar(Vector2.up);
+            else if (Input.GetKey(KeyCode.DownArrow)) Disparar(Vector2.down);
+            else if (Input.GetKey(KeyCode.LeftArrow)) Disparar(Vector2.left);
+            else if (Input.GetKey(KeyCode.RightArrow)) Disparar(Vector2.right);
         }
     }
 
-    /// <summary>
-    /// Instancia el cubo proyectil y le asigna velocidad física en la dirección de la flecha presionada.
-    /// </summary>
     private void Disparar(Vector2 direccion)
     {
         if (prefabProyectil != null)
         {
-            // Creamos el proyectil
             GameObject proyectil = Instantiate(prefabProyectil, transform.position, Quaternion.identity);
-            
-            // Accedemos a su Rigidbody2D para impulsarlo en la dirección indicada
             Rigidbody2D rbProyectil = proyectil.GetComponent<Rigidbody2D>();
             if (rbProyectil != null)
-            {
                 rbProyectil.velocity = direccion * velocidadProyectil;
-            }
-
             tiempoSiguienteDisparo = Time.time + cadenciaDisparo;
         }
     }
@@ -87,17 +51,16 @@ public class Player : MonoBehaviour
     public void RecibirDano(int cantidad)
     {
         vidaActual -= cantidad;
-        Debug.Log("¡Jugador recibió " + cantidad + " de daño! Vida restante: " + vidaActual);
-
+        if (uiVidas != null)
+            uiVidas.ActualizarVidas(vidaActual, vidaMaxima);
         if (vidaActual <= 0)
-        {
             Morir();
-        }
     }
 
     private void Morir()
     {
-        Debug.Log("El jugador ha sido eliminado. Reiniciando nivel...");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (GameManager.Instance != null)
+            GameManager.Instance.MostrarMenuMuerte();
+        gameObject.SetActive(false);
     }
 }
